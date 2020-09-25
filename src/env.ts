14ef0be6ae42b5f147fdefbc67aa57689aaf29d1
lostfictions <lostfictions@users.noreply.github.com>
2020-09-25 02:51:27 -0400

    add sentry

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,32 @@
 import envalid from "envalid";
+import * as Sentry from "@sentry/node";
+import { CaptureConsole } from "@sentry/integrations";
 
-const env = envalid.cleanEnv(
+export const {
+  MASTODON_SERVER,
+  MASTODON_TOKEN,
+  SENTRY_DSN,
+  isDev,
+} = envalid.cleanEnv(
   process.env,
   {
     MASTODON_SERVER: envalid.url({ default: "https://mastodon.social" }),
     MASTODON_TOKEN: envalid.str(),
+    SENTRY_DSN: envalid.str({ default: "" }),
   },
   { strict: true }
 );
 
-export const { MASTODON_SERVER, MASTODON_TOKEN } = env;
+if (SENTRY_DSN.length === 0) {
+  console.warn(
+    `Sentry DSN is invalid! Error reporting to Sentry will be disabled.`
+  );
+} else {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    environment: isDev ? "dev" : "prod",
+    integrations: [
+      new CaptureConsole({ levels: ["warn", "error", "debug", "assert"] }),
+    ],
+  });
+}
