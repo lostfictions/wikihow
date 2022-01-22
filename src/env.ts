453c36a2bbcc957fc5777390fb19599d7ed182f5
lostfictions <lostfictions@users.noreply.github.com>
2022-01-21 23:52:52 -0500

    switch to znv

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,12 @@
-import envalid from "envalid";
 import * as Sentry from "@sentry/node";
 import { CaptureConsole } from "@sentry/integrations";
+import { parseEnv, z } from "znv";
+
+const isDev = process.env["NODE_ENV"] !== "production";
+
+if (isDev) {
+  require("dotenv").config();
+}
 
 export const {
   MASTODON_TOKEN,
@@ -10,20 +16,15 @@ export const {
   TWITTER_ACCESS_TOKEN,
   TWITTER_ACCESS_SECRET,
   SENTRY_DSN,
-  isDev,
-} = envalid.cleanEnv(
-  process.env,
-  {
-    MASTODON_TOKEN: envalid.str(),
-    MASTODON_TOKEN_ORIG: envalid.str(),
-    TWITTER_API_KEY: envalid.str(),
-    TWITTER_API_SECRET: envalid.str(),
-    TWITTER_ACCESS_TOKEN: envalid.str(),
-    TWITTER_ACCESS_SECRET: envalid.str(),
-    SENTRY_DSN: envalid.str({ default: "" }),
-  },
-  { strict: true }
-);
+} = parseEnv(process.env, {
+  MASTODON_TOKEN: z.string().nonempty(),
+  MASTODON_TOKEN_ORIG: z.string().nonempty(),
+  TWITTER_API_KEY: z.string().nonempty(),
+  TWITTER_API_SECRET: z.string().nonempty(),
+  TWITTER_ACCESS_TOKEN: z.string().nonempty(),
+  TWITTER_ACCESS_SECRET: z.string().nonempty(),
+  SENTRY_DSN: z.string().nonempty().optional(),
+});
 
 /** account to which to toot images with a random caption from a different article. */
 export const MASTODON_SERVER = "https://mastodon.social";
@@ -31,7 +32,7 @@ export const MASTODON_SERVER = "https://mastodon.social";
 /** alternate account to which to toot images with their original caption. */
 export const MASTODON_SERVER_ORIG = "https://botsin.space";
 
-if (SENTRY_DSN.length === 0) {
+if (!SENTRY_DSN && !isDev) {
   console.warn(
     `Sentry DSN is invalid! Error reporting to Sentry will be disabled.`
   );
